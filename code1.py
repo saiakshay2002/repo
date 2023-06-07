6f31285185d5bc37b86b5145ffc06267869fd08e
sai akshay <bandaru.saiakshay@gmail.com>
2023-06-07 16:10:53 +0530

    repo-1

---
+++ code1.py
@@ -0,0 +1,20 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+img =cv2.imread("/home/iwizards/Desktop/pythonfolder/cv1.PNG",1)
+#img=cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+
+height=img.shape
+width=img.shape[1]
+#channels=img.shape[2]
+si=img.size
+print("height",height)
+print("wid",width)
+#print("chann",channels)
+print("size",si)
+#plt.imshow(img)
+#cv2.imshow("image1",img)
+
+cv2.imshow("image",img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()

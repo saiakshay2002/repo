b3b808677badcd6e7c8c4f3d9d26a4829af64459
sai akshay <bandaru.saiakshay@gmail.com>
2023-06-07 18:18:18 +0530

    initial_commit

---
+++ code1.py
@@ -0,0 +1,15 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+img =cv2.imread("/home/iwizards/Desktop/pythonfolder/cv1.PNG",1)
+#img=cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+
+height=img.shape
+width=img.shape[1]
+#channels=img.shape[2]
+si=img.size
+print("height",height)
+print("wid",width)
+
+
+
